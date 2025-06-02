afde704b75a9390ab965170155cb103a1299e488
Sarocy <sarahocy@hotmail.com>
2025-06-01 22:55:25 -0300

    projeto

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+
+import './globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata: Metadata = {
+  title: 'Ocy Hilink',
+  description: 'Aplicativo UI/UX de viagem para acampamento',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'Como o Hilink funciona?' },
+  { href: '/', key: 'services', label: 'Serviços' },
+  { href: '/', key: 'pricing ', label: 'Preços' },
+  { href: '/', key: 'contact_us', label: 'Contato' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Mapas que podem estar offline',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'Nós fornecemos uma solução para que você possa usar nosso aplicativo durante a trilha, mapas offline que você pode usar a qualquer hora que não houver sinal no local',
+  },
+  {
+    title: 'Defina um cronograma de aventura',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Programe uma aventura com os amigos. Nos feriados, a Hilink oferece muitas ofertas interessantes. Assim, não há mais discussão.",
+  },
+  {
+    title: 'Tecnologia usando realidade aumentada',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'A tecnologia usa a realidade aumentada como guia para sua trilha na floresta. Já com o suporte da mais recente tecnologia sem conexão à internet.',
+  },
+  {
+    title: 'Muitos novos locais todos os meses',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Muitos novos locais todos os meses, porque temos uma comunidade mundial de trilheiros que compartilham suas melhores experiências com a gente.',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Aprenda mais',
+    links: [
+      'Sobre o Hilink',
+      'Comunicados de imprensa',
+      'Ambiente',
+      'Trabalhos',
+      'Politicas de privacidade',
+      'Contato',
+    ],
+  },
+  {
+    title: 'Nossa comunidade',
+    links: ['Trilheiros.com', 'Na trilha hilink', 'Hilink unidos'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contato',
+  links: [
+    { label: 'Diretor administrativo', value: '1234-5678' },
+    { label: 'Email', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
